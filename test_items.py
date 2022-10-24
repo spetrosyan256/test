25a253e98ccb81d4177a62bc41645b1f5ffe9938
spetrosyan256 <116346717+spetrosyan256@users.noreply.github.com>
2022-10-24 20:40:48 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,5 +5,6 @@ link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_different_languages(browser):
     browser.get(link)
     nahodka = browser.find_elements(By.XPATH, '//button[@type="submits"]')
-    assert nahodka, 'Oh my GOSH! Button is gone! T_T'
+    print(len(nahodka))
+    assert len(nahodka)>0, 'Oh my GOSH! Button is gone! T_T'
     time.sleep(30)
